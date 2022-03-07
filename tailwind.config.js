33f7ee83db8d765df17f84acc8775c6d710144e9
TRONG <tronginc@gmail.com>
2022-03-07 23:16:51 +0700

    enter app

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,18 @@
 module.exports = {
   content: ["./docs/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      keyframes: {
+        scale: {
+          "0%": { transform: "scale(1)" },
+          "50%": { transform: "scale(1.05)" },
+          "0%": { transform: "scale(1)" },
+        },
+      },
+      animation: {
+        scale: "scale 1.5s ease-in-out infinite",
+      },
+    },
   },
   plugins: [],
 };
