ef3c2b04dae4017ef3d014498b9f09b91c563342
TRONG <tronginc@gmail.com>
2022-03-07 22:04:04 +0700

    add black cover

--- docs/assets/js/main.js
+++ docs/assets/js/main.js
@@ -1,17 +1,3 @@
 window.addEventListener("DOMContentLoaded", onload);
 
-function overideFrameStyles() {
-  const new_style_element = document.createElement("style");
-  new_style_element.textContent = ".controls { display: none !importance; }";
-  const interval = setInterval(() => {
-    const frame = document.getElementById("iframe");
-    if (frame.contentDocument) {
-      frame.contentDocument.head.appendChild(new_style_element);
-      clearInterval(interval);
-    }
-  }, 1000);
-}
-
-function onload() {
-  // overideFrameStyles();
-}
+function onload() {}
